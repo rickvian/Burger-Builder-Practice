4e30c031d0104a5725309a5edce1c89936b94f1c
rickvian <rickvianaldi@gmail.com>
2020-10-25 11:01:50 +0700

    adding routing to burger

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+
+import classes from './CheckoutSummary.module.css';
+
+
+const CheckoutSummary = props => {
+    return (
+        <div className={classes.CheckoutSummary}>
+            <h1>
+                We hope it tastes well!
+            </h1>
+            <div style={{width:'100%'`, margin: 'auto'}}>
+                <Burger ingredients={props.ingredients}/>
+            </div>
+            <Button btnType="Danger" clicked>Cancel</Button>
+            <Button btnType="Success" clicked>Continue</Button>
+        </div>
+    );
+};
+
+CheckoutSummary.propTypes = {
+    
+};
+
+export default CheckoutSummary;
