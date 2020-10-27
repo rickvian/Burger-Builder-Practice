02a4147ce56ab63899e547c28a6b729f71be5e92
rickvian <rickvianaldi@gmail.com>
2020-10-27 08:24:24 +0700

    Setting Up a JS Config for the Form-

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classes from "./Input.module.css";
+
+const Input = (props) => {
+    let inputElement = null;
+    switch (props.elementType) {
+        case "input":
+            inputElement = (
+                <input
+                    className={classes.InputElement}
+                    {...props.elementConfig}
+                    value={props.value}
+                />
+            );
+            break;
+        case "textarea":
+            inputElement = (
+                <textarea
+                    className={classes.InputElement}
+                    {...props.elementConfig}
+                    value={props.value}
+                />
+            );
+            break;
+        default:
+            inputElement = (
+                <input
+                    className={classes.InputElement}
+                    {...props.elementConfig}
+                    value={props.value}
+                />
+            );
+    }
+
+    return (
+        <div className={classes.Input}>
+            <label className={classes.Label}>{props.label}</label>
+            {inputElement}
+        </div>
+    );
+};
+
+Input.propTypes = {};
+
+export default Input;
